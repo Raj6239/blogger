00a98d7e32dff336df7e5ad2c98e043ae9f3eef7
Poplify <poplify>
2015-02-02 17:11:52 +0530

    form based workflow completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,42 @@ class ArticlesController < ApplicationController
 		
 	end
 
-	def new
-		
+	def edit
+  		@article = Article.find(params[:id])
 	end
+
+
+	def new
+		@article= Article.new
+	end
+   
+	def update
+  		@article = Article.find(params[:id])
+  		@article.update(article_params)
+
+  		 flash.notice = "Your Article '#{@article.title}' Updated!"
+
+  			redirect_to article_path(@article)
+	end
+
+
+	def create
+		@article = Article.new
+		@article.title = params[:article][:title]
+		@article.body = params[:article][:body]
+		@article.save
+		   flash.notice = " Article '#{@article.title}' created successfully !"	
+			redirect_to article_path(@article)
+
+	end
+
+	def destroy
+		@article= Article.find(params[:id])
+		@article.destroy
+			flash.notice = " Article '#{@article.title}' destroy successfully !"	
+			redirect_to articles_path
+	end
+
+
+	include ArticlesHelper
 end

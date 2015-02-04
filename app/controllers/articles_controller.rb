e800c0578cf4d567d7894bc7737d513bf450e9c2
Poplify <poplify>
2015-02-04 11:50:55 +0530

    few gems completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,9 +24,14 @@ class ArticlesController < ApplicationController
 	def update
 
   		@article = Article.find(params[:id])
-
   		@article.update(article_params)
-	
+
+  		params[:images].each do |image|
+		@attachment=Attachment.new
+		@attachment.article_id=@article.id
+		@attachment.image=image
+		@attachment.save
+	   end
   		 flash.notice = "Your Article '#{@article.title}' Updated!"
 
   			redirect_to article_path(@article)
@@ -37,8 +42,19 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 		@article.title = params[:article][:title]
 		@article.body = params[:article][:body]
+		@article.tag_list = params[:article][:tag_list]
+		@article.photo = params[:article][:photo]
 		@article.save
+		
+		params[:images].each do |image|
+		@attachment=Attachment.new
+		@attachment.article_id=@article.id
+		@attachment.image=image
+		@attachment.save
+		end
+
 		   flash.notice = " Article '#{@article.title}' created successfully !"	
+		
 			redirect_to article_path(@article)
 
 	end
